520d1a345c7d395e21b4d0ad84c238ef13cf1d55
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-24 15:11:25 -0300

    Add files via upload

---
+++ Exercicio6ArrayList.java
@@ -0,0 +1,22 @@
+
+package exercicio6.arraylist;
+
+import java.util.ArrayList;
+
+
+public class Exercicio6ArrayList {
+
+    
+    public static void main(String[] args) {
+        
+        ArrayList <Integer> palavras = new ArrayList<>();
+        Integer palavra1 = null;
+        
+        palavras.add(palavra1.SIZE);
+        palavras.addAll(palavras);
+       
+        System.out.println("Insira um palavra:" + palavras + palavras);
+        
+    }
+    
+}
